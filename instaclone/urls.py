c8da143896108691ca6170992266a32fd7a7efe4
Tellvinch <tellvinchimani@gmail.com>
2020-07-06 10:46:41 +0300

    add changes

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -1,6 +1,7 @@
-"""Instacole URL Configuration
+"""instaclone URL Configuration
+
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -12,13 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Pics.urls')),
 ]
